4637a4852fba9309bfe730148010372b15b94c6f
Слава <nterehina666@mail.ru>
2021-03-02 19:49:22 +0300

    Wrote CreatePizzaController and some html code

--- src/main/java/com/example/demo/CreatePizzaController.java
+++ src/main/java/com/example/demo/CreatePizzaController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -17,10 +20,34 @@ public class CreatePizzaController {
     public String showDesignForm(Model model){
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("CLS22", "Classic base 22cm", Ingredient.Type.BASIC),
-                new Ingredient("PEPE", "Pepperoni", Ingredient.Type.MEAT)
+                new Ingredient("CLS30", "Classic base 30cm", Ingredient.Type.BASIC),
+                new Ingredient("PEPE", "Pepperoni", Ingredient.Type.MEAT),
+                new Ingredient("HAM", "Ham", Ingredient.Type.MEAT),
+                new Ingredient("CHMPG", "Champignons", Ingredient.Type.VEGGIES),
+                new Ingredient("CUCU", "Cucumbers", Ingredient.Type.VEGGIES),
+                new Ingredient("PARM", "Parmesan cheese", Ingredient.Type.CHEESE),
+                new Ingredient("MOZ", "Mozzarella cheese", Ingredient.Type.CHEESE),
+                new Ingredient("BARBS", "Barbecue sauce", Ingredient.Type.SAUCE),
+                new Ingredient("TOMAS", "Tomato sauce", Ingredient.Type.SAUCE)
                 );
 
-        return "design";
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for(Ingredient.Type type: types){
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+        model.addAttribute("create", new Pizza());
+        //model.addAttribute("basic", new Pizza());
+        return "create";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+        List<Ingredient> filtered = new ArrayList<>();
+        for (Ingredient ingredient:ingredients){
+            if(ingredient.getType() == type){
+                filtered.add(ingredient);
+            }
+        }
+        return filtered;
     }
 }
 
