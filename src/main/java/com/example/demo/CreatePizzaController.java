482fbaf9921598424492e97405cb6aef06c51d6f
Слава <nterehina666@mail.ru>
2021-03-11 23:20:27 +0300

    Validation completed

--- src/main/java/com/example/demo/CreatePizzaController.java
+++ src/main/java/com/example/demo/CreatePizzaController.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -43,8 +45,8 @@ public class CreatePizzaController {
     }
 
     @PostMapping
-    public String processCreation(@Valid Pizza pizza, Errors errors){
-        if(errors.hasErrors()){
+    public String processCreation(@Valid @ModelAttribute("create") Pizza pizza, BindingResult result){
+        if(result.hasErrors()){
             return "create";
         }
         //Save creation...
--- src/main/java/com/example/demo/OrderController.java
+++ src/main/java/com/example/demo/OrderController.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,8 +22,8 @@ public class OrderController {
         return "orderForm";
     }
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
-        if (errors.hasErrors()){
+    public String processOrder(@Valid @ModelAttribute("order") Order order, BindingResult result) {
+        if (result.hasErrors()){
             return "orderForm";
         }
         log.info("Order submitted: " + order);
